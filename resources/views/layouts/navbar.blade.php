efd4c7ed9c15a4859dabf40e4e9e821ce3bdd3ab
zaheralmattrud <zaheralmattrud@gmail.com>
2023-05-14 14:01:07 +0300

    add fortify config , auth routes

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,37 +1,53 @@
 <nav class="navbar navbar-expand-lg bg-white rounded shadow-sm">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Blog </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          {{-- <li class="nav-item">
+        <a class="navbar-brand" href="#">Blog </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                {{-- <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
           </li> --}}
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Account
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Login</a></li>
-              <li><a class="dropdown-item" href="#">Register</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Alex</a></li>
-              <li><a class="dropdown-item" href="#">Logout</a></li>
-            </ul>
-          </li>
-          {{-- <li class="nav-item">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Account
+                    </a>
+                    <ul class="dropdown-menu">
+
+                        @guest
+                            <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+                            <li><a class="dropdown-item" href="{{ route('register') }}">Register</a></li>
+                        @endguest
+                        @auth
+
+                            <li><a class="dropdown-item" href="#">{{ auth()->user()->name }}</a></li>
+                            <li><a class="dropdown-item" href="#"
+                                    onclick="document.getElementById('formLogout').submit()">Logout</a></li>
+                            <form id="formLogout" action="{{ route('logout') }}" method="POST">
+                                @csrf
+
+                            </form>
+
+
+                        @endauth
+
+
+                    </ul>
+                </li>
+                {{-- <li class="nav-item">
             <a class="nav-link disabled">Disabled</a>
           </li> --}}
-        </ul>
-        {{-- <form class="d-flex" role="search">
+            </ul>
+            {{-- <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form> --}}
-      </div>
+        </div>
     </div>
-  </nav>
+</nav>
