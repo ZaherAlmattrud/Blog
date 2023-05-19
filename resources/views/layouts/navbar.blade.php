85e99295153abb0c764fbcbce207d48953468016
unknown <zaheralmattrud@gmail.com>
2023-05-19 06:41:24 -0700

    install and use fontawesome library

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,7 +8,8 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">
+                        <i class="fas fa-home"></i> Home</a>
                 </li>
                 {{-- <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
@@ -16,19 +17,19 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
-                        Account
+                       <i class="fas fa-users"></i> Account
                     </a>
                     <ul class="dropdown-menu">
 
                         @guest
-                            <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="dropdown-item" href="{{ route('register') }}">Register</a></li>
+                            <li><a class="dropdown-item" href="{{ route('login') }}"> <i class="fas fa-sign-in"></i> Login</a></li>
+                            <li><a class="dropdown-item" href="{{ route('register') }}"> <i class="fas fa-user-plus"></i> Register</a></li>
                         @endguest
                         @auth
 
                             <li><a class="dropdown-item" href="#">{{ auth()->user()->name }}</a></li>
                             <li><a class="dropdown-item" href="#"
-                                    onclick="document.getElementById('formLogout').submit()">Logout</a></li>
+                                    onclick="document.getElementById('formLogout').submit()"><i class="fas fa-sign-out"></i> Logout</a></li>
                             <form id="formLogout" action="{{ route('logout') }}" method="POST">
                                 @csrf
 
