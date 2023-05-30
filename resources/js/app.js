742c63eec157c0c8dcc9b0cafadaf324d23e214f
zaheralmattrud <zaheralmattrud@gmail.com>
2023-05-30 13:26:14 +0300

    install pinana vue js library for store state

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,14 @@
 import './bootstrap';
 
 import {createApp} from 'vue/dist/vue.esm-bundler.js'
+import { createPinia } from 'pinia'
 import  Comments  from './components/Comments.vue'
 import  AddComment  from './components/AddComment.vue'
 import  CommentsCount  from './components/CommentsCount.vue'
 
 const app  = createApp({});
+const pinia = createPinia()
+app.use(pinia);
 
 app.component("comments-component" , Comments);
 app.component("add-comment-component" , AddComment);
