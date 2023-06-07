ab139a8310da4c252a19c941ec92021de00dc649
zaheralmattrud <zaheralmattrud@gmail.com>
2023-06-07 13:47:19 +0300

    add admin apis : loginForm , login , logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+
+
+    public function loginForm()
+    {
+
+        if (auth()->guard('admin')->check()) {
+
+            return redirect()->back();
+        }
+
+        return view('admin.auth.login');
+    }
+
+
+    public function login(Request $request){
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required|min:6'
+        ]);
+
+        if(auth()->guard('admin')->attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])){
+            return redirect()->route('admin.index');
+        }else{
+            return redirect()->route('admin.loginForm')->with([
+                'error' => 'These credentials do not match our records'
+            ]);
+        }
+    }
+
+    public function logout(){
+        auth()->guard('admin')->logout();
+        return redirect()->route('admin.loginForm');
+    }
 }
